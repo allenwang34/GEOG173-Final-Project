ed7596791f21842bf66a2d824a42924dc36af8cf
AllenWang <allenwang950321@gmail.com>
2017-05-13 11:05:35 -0700

    Write down a general logic for the first step of our project. I'm currently working on create a user point location on the map.

---
+++ GEOG173_Final_Project/GEOG173_Final_Project/GEOG173_Final_Project.py
@@ -0,0 +1,8 @@
+#Author: Yiyang Wang
+
+import arcpy
+import math
+
+#The first thing to do is let the user enter his or her location, X is longitude and Y is latitude. 
+#Bascially we create a point geometry on the map. 
+
