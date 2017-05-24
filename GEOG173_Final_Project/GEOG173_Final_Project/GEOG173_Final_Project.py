4018ad4b117057958a27cb2ab08738d2c44ba5e0
AllenWang <allenwang950321@gmail.com>
2017-05-24 16:42:32 -0700

    Now we can create a user location on a duplicated shapefile based on user input.

--- GEOG173_Final_Project/GEOG173_Final_Project/GEOG173_Final_Project.py
+++ GEOG173_Final_Project/GEOG173_Final_Project/GEOG173_Final_Project.py
@@ -14,7 +14,7 @@ arcpy.env.overwriteOutput = True
 #choose a shapefile to workwith 
 inStations = arcpy.GetParameterAsText(1) #this can be a feature class
 outStations = arcpy.GetParameterAsText(2) #this is a duplicated point layer, we will create user location on this layer
-results = arcpy.GetParameterAsText(3) #This shape file only includes the result stations and their distances&travel time to the user.
+#results = arcpy.GetParameterAsText(3) #This shape file only includes the result stations and their distances&travel time to the user.
 #we will create map book from this result layer. 
 
 
@@ -22,6 +22,37 @@ results = arcpy.GetParameterAsText(3) #This shape file only includes the result 
 #The first step to do is let the user enter his or her location, X is longitude and Y is latitude. 
 #Bascially we create a point geometry on the map. 
 arcpy.CopyFeatures_management(inStations, outStations)
+userCoord = arcpy.GetParameterAsText(3) #get the user location 
+Coords = userCoord.split(" ")
+xCoord = float(Coords[0])
+yCoord = float(Coords[1])
+
+cursor = arcpy.da.InsertCursor(outStations,["SHAPE@XY"])
+xy = (xCoord,yCoord)
+cursor.insertRow([xy])
+del cursor
+
+infoInserter = arcpy.da.UpdateCursor(outStations,["Station_Na","Latitude","Longitude"])
+points = arcpy.SearchCursor(outStations)
+
+for point in points:
+    pointNam = point.getValue("Station_Na")
+    if pointNam == " ":
+        pointIndex = point.getValue("FID")
+
+rowCounter = 0
+for row in infoInserter:
+    if rowCounter == pointIndex:
+        row[0] = "UserLocation"
+        row[1] = yCoord
+        row[2] = xCoord
+        infoInserter.updateRow(row)
+        break
+    rowCounter += 1
+
+del points,point,infoInserter,row
+
+
 
 
 
