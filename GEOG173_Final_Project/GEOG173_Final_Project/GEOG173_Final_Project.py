b24df1b750e45d959136597d6c8954b62f2baebd
AllenWang <allenwang950321@gmail.com>
2017-06-04 14:11:56 -0700

    Unkonwn errors occurs.

--- GEOG173_Final_Project/GEOG173_Final_Project/GEOG173_Final_Project.py
+++ GEOG173_Final_Project/GEOG173_Final_Project/GEOG173_Final_Project.py
@@ -13,7 +13,7 @@ folder_path = arcpy.GetParameterAsText(0)
 arcpy.env.workspace = folder_path
 arcpy.env.overwriteOutput = True
 
-#choose a shapefile to workwith 
+###choose a shapefile to workwith 
 charging_stations = folder_path + r'\Charging_Stations.shp' 
 
 #setup new file names and locations
@@ -27,7 +27,7 @@ arcpy.CopyFeatures_management(charging_stations, result_stations)
 
 
 #ask for user defined location
-#sample location: x = -118.439298  y = 34.073990
+#sample location: x = -118.442301  y = 34.071667
 userCoord = arcpy.GetParameterAsText(1) #get the user location 
 
 #after user type in the xy location, extract x and y value 
@@ -35,17 +35,17 @@ Coords = userCoord.split(" ")
 xCoord = float(Coords[0]) #make xy to float type
 yCoord = float(Coords[1])
 
-#make a tuple
+#make a tuple of coordinates
 xy = (xCoord,yCoord)
 
-#create a point geometry in one of the duplicated file 
+#create a point cursor to add xy point to shapefile copy 
 pointCursor = arcpy.da.InsertCursor(charging_stations_copy,["SHAPE@XY"])
 pointCursor.insertRow([xy])
 del pointCursor
 
 
 
-#create a single point shapefile
+#create a single point shapefile based off copy of shapefile
 arcpy.CopyFeatures_management(charging_stations_copy, single_point)
 
 #remove extra rows from single_point shapefile
@@ -83,50 +83,63 @@ del cursor, row
 
 #we condcut facility network analysis here
 
-arcpy.CheckOutExtension('Network')
+#check out extension (although it does not currently work)
+#you must manually select
+if arcpy.CheckExtension("Network") == "Available":
+    arcpy.CheckOutExtension("Network")   
 
-   
-
+#create file path info for incidents and facilities
 inIncidents  = folder_path + r'\Results\User Location.shp'
-inFacilities = folder_path + r'\Results\Charging_Stations_Copy.shp'
+inFacilities = folder_path + r'\Results\Nearest Stations.shp'
 
+#create a closest facility layer that
 closest_layer = arcpy.MakeClosestFacilityLayer_na(in_network_dataset=folder_path + r'\Geog170_Street_Dataset\streets',
                                   out_network_analysis_layer="Closest Facility", impedance_attribute="Time", travel_from_to="TRAVEL_TO", default_cutoff="",
                                   default_number_facilities_to_find="3", accumulate_attribute_name="", UTurn_policy="ALLOW_UTURNS",
                                   restriction_attribute_name="TurnRestriction;OneWay", hierarchy="USE_HIERARCHY", hierarchy_settings="",
                                   output_path_shape="TRUE_LINES_WITH_MEASURES", time_of_day="", time_of_day_usage="NOT_USED")
 
+#create a way to reference the closest facility layer
 out_CL = closest_layer.getOutput(0)
 
+#select the sublayers
 subLayerNames = arcpy.na.GetNAClassNames(out_CL)
 facilitiesLayerName = subLayerNames["Facilities"]
 incidentsLayerName = subLayerNames["Incidents"]
 
+#add facilities to facilities layer in the closest facility object layer
 arcpy.na.AddLocations(out_CL,facilitiesLayerName,inFacilities,"","")
+#add incidents to incidents layer in the closest facility object layer
 arcpy.na.AddLocations(out_CL,incidentsLayerName,inIncidents,"","")
 
+#create a folder path for the output of facilities layer
 out_layer = folder_path + r'\Results\OUTPUT_LAYER.lyr'
 
-
-
+#solve our closest facilities layer
+#skip any errors
 arcpy.na.Solve(out_CL,"SKIP")
 
+#save a copy of our closest facility layer as a layerfile to our folder
 out_CL.saveACopy(out_layer)
 
-
-routesLayer = out_layer + r'\Closest Facility\Routes'
+#folder path of routes to be used as the output shapefile for easy manipulation
 routesShape = folder_path + r'\Results\Routes.shp'
 
+#object to be used to select a layerfile
 layerFile = arcpy.mapping.Layer(out_layer)
+#object to be used to select a specific layer in the layerfile
 layers = arcpy.mapping.ListLayers(layerFile)
 
+#search through each sublayer and if sublayer is called routes
+#create a shapefile of the sublayer
 for layer in layers:
     if layer.name == "Routes":
         arcpy.CopyFeatures_management(layer, routesShape)
 
+#check in the extension
+arcpy.CheckInExtension("Network")
 
 
-arcpy.CheckInExtension('Network')
 
 #Create a shapefile of selected stations from route shapefile
 
@@ -270,14 +283,14 @@ except OSError:
 
 
 #arcpy.CheckOutExtension('Spatial')
-OutputKernel = folder_path + r'\FuelTypeKernel.tif'
-arcpy.gp.KernelDensity_sa(charging_stations_copy, "NONE", 
-                          OutputKernel,
-                          "0.1857694464", "", "SQUARE_MAP_UNITS",
-                          "DENSITIES", "PLANAR")
-addlayer = arcpy.mapping.Layer(OutputKernel)
-arcpy.mapping.AddLayer(df, addlayer)
-arcpy.ApplySymbologyFromLayer_management("FuelTypeKernel.tif", "Kernel.tif")
+#OutputKernel = folder_path + r'\FuelTypeKernel.tif'
+#arcpy.gp.KernelDensity_sa(charging_stations_copy, "NONE", 
+#                          OutputKernel,
+#                          "0.1857694464", "", "SQUARE_MAP_UNITS",
+#                          "DENSITIES", "PLANAR")
+#addlayer = arcpy.mapping.Layer(OutputKernel)
+#arcpy.mapping.AddLayer(df, addlayer)
+#arcpy.ApplySymbologyFromLayer_management("FuelTypeKernel.tif", "Kernel.tif")
 
 
 
